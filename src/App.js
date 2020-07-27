bf6448f768ae343eb6d754841bf8641e0ba633ae
barretorodrigo <barretorodrigo@gmail.com>
2020-07-27 20:28:37 -0300

    My datas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Menu from './components/Menu'
-import dadosIniciais from './data/dados_iniciais.json';
+import dadosIniciais from './data/meus_dados.json';
 import BannerMain from './components/BannerMain';
 import Carousel from './components/Carousel';
 import Footer from './components/Footer';
@@ -10,11 +10,11 @@ function App() {
     <div style={{ background: "#141414" }}>
       <Menu />
 
-      <BannerMain
+      {/* <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
-      />
+      /> */}
 
       <Carousel
         ignoreFirstVideo
@@ -25,21 +25,7 @@ function App() {
         category={dadosIniciais.categorias[1]}
       />
 
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />      
-
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />      
-
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      />      
-
-      <Carousel
-        category={dadosIniciais.categorias[5]}
-      />      
+     
 
       <Footer />
     </div>
